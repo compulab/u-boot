andreimironenko/u-boot-davinci

3517: musb: MSC host support
Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>

drivers/usb/musb/musb_core.c
@@ -142,11 +142,33 @@ void write_fifo(u8 ep, u32 length, void *fifo_data)
 void read_fifo(u8 ep, u32 length, void *fifo_data)
 {
 	u8  *data = (u8 *)fifo_data;
+#ifdef CONFIG_OMAP3_OMAP3517EVM
+	int i;
+	u32 val;
+#endif
 
 	/* select the endpoint index */
 	writeb(ep, &musbr->index);
 
 	/* read the data to the fifo */
+#ifdef CONFIG_OMAP3_OMAP3517EVM
+	/* AM3517 FIFO should be read double word wise as bytewise
+	 * FIFO read corrupts the FIFO
+	 */
+	if (length > 4) {
+		for (i = 0; i < (length >> 2); i++) {
+			val = readl(&musbr->fifox[ep]);
+			memcpy(data, &val, 4);
+			data += 4;
+		}
+		length %= 4;
+	}
+	if (length > 0) {
+		val = readl(&musbr->fifox[ep]);
+		memcpy(data, &val, length);
+	}
+#else
 	while (length--)
 		*data++ = readb(&musbr->fifox[ep]);
+#endif
 }

drivers/usb/musb/musb_udc.c
@@ -57,6 +57,8 @@
 #include "musb_core.h"
 #if defined(CONFIG_USB_OMAP3)
 #include "omap3.h"
+#elif defined(CONFIG_USB_OMAP3517)
+#include "am3517.h"
 #elif defined(CONFIG_USB_DAVINCI)
 #include "davinci.h"
 #endif

include/usb.h
@@ -132,7 +132,7 @@ struct usb_device {
 	defined(CONFIG_USB_EHCI) || defined(CONFIG_USB_OHCI_NEW) || \
 	defined(CONFIG_USB_SL811HS) || defined(CONFIG_USB_ISP116X_HCD) || \
 	defined(CONFIG_USB_R8A66597_HCD) || defined(CONFIG_USB_DAVINCI) || \
-	defined(CONFIG_USB_OMAP3)
+	defined(CONFIG_USB_OMAP3) || defined(CONFIG_USB_OMAP3517)
 
 int usb_lowlevel_init(void);
 int usb_lowlevel_stop(void);

compulab/u-boot

drivers/usb/musb/musb_core.c
@@ -142,11 +142,33 @@ void write_fifo(u8 ep, u32 length, void *fifo_data)
 void read_fifo(u8 ep, u32 length, void *fifo_data)
 {
 	u8  *data = (u8 *)fifo_data;
+#ifdef CONFIG_OMAP3_AM3517EVM
+	int i;
+	u32 val;
+#endif
 
 	/* select the endpoint index */
 	writeb(ep, &musbr->index);
 
 	/* read the data to the fifo */
+#ifdef CONFIG_OMAP3_AM3517EVM
+	/* AM3517 FIFO should be read double word wise as bytewise
+	 * FIFO read corrupts the FIFO
+	 */
+	if (length > 4) {
+		for (i = 0; i < (length >> 2); i++) {
+			val = readl(&musbr->fifox[ep]);
+			memcpy(data, &val, 4);
+			data += 4;
+		}
+		length %= 4;
+	}
+	if (length > 0) {
+		val = readl(&musbr->fifox[ep]);
+		memcpy(data, &val, length);
+	}
+#else
 	while (length--)
 		*data++ = readb(&musbr->fifox[ep]);
+#endif
 }

drivers/usb/musb/musb_udc.c
@@ -57,6 +57,8 @@
 #include "musb_core.h"
 #if defined(CONFIG_USB_OMAP3)
 #include "omap3.h"
+#elif defined(CONFIG_USB_AM3517)
+#include "am3517.h"
 #elif defined(CONFIG_USB_DAVINCI)
 #include "davinci.h"
 #endif

include/usb.h
@@ -132,7 +132,7 @@ struct usb_device {
 	defined(CONFIG_USB_EHCI) || defined(CONFIG_USB_OHCI_NEW) || \
 	defined(CONFIG_USB_SL811HS) || defined(CONFIG_USB_ISP116X_HCD) || \
 	defined(CONFIG_USB_R8A66597_HCD) || defined(CONFIG_USB_DAVINCI) || \
-	defined(CONFIG_USB_OMAP3)
+	defined(CONFIG_USB_OMAP3) || defined(CONFIG_USB_AM3517)
 
 int usb_lowlevel_init(void);
 int usb_lowlevel_stop(void);