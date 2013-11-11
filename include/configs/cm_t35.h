41089b13bd4e5667255d23816481f621787d351d
Nikita Kiryanov <nikita@compulab.co.il>
2013-11-11 17:30:46 +0200

    cm_t35: remove debug option from bootargs
    Remove "omapfb.debug=y" from default environment's Linux bootargs for
    both nand and mmc boot.

--- include/configs/cm_t35.h
+++ include/configs/cm_t35.h
@@ -192,7 +192,6 @@
 		"mpurate=${mpurate} " \
 		"vram=${vram} " \
 		"omapfb.mode=dvi:${dvimode} " \
-		"omapfb.debug=y " \
 		"omapdss.def_disp=${defaultdisplay} " \
 		"root=${mmcroot} " \
 		"rootfstype=${mmcrootfstype}\0" \
@@ -200,7 +199,6 @@
 		"mpurate=${mpurate} " \
 		"vram=${vram} " \
 		"omapfb.mode=dvi:${dvimode} " \
-		"omapfb.debug=y " \
 		"omapdss.def_disp=${defaultdisplay} " \
 		"root=${nandroot} " \
 		"rootfstype=${nandrootfstype}\0" \
