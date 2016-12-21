343caea96996151778a6b22d49db12f2f86d5462
Valentin Raevsky <valentin@compulab.co.il>
2016-12-21 16:37:38 +0200

    arm: mx6: cl_som_mx6: Add basic support
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>
    
    Conflicts:
    	arch/arm/Kconfig

---
+++ include/configs/cl-som-imx6.h
@@ -0,0 +1,16 @@
+/*
+ * Config file for Compulab CM-FX6 board
+ *
+ * Copyright (C) 2016, Compulab Ltd - http://compulab.co.il/
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_CL_SOM_IMX6_H
+#define __CONFIG_CL_SOM_IMX6_H
+
+#include "cm_fx6.h"
+
+#define CL_SOM_IMX6
+
+#endif	/* __CONFIG_CL_SOM_IMX6_H */
