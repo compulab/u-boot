0fbc22d16ef4fd7441aece04542de2c52ec649bb
Ilya Ledvich <ilya@compulab.co.il>
2017-04-26 13:46:52 +0300

    arm: imx7d: cl-som-imx7: add support for I2C4 bus (cont.)
    Add CONFIG_SYS_I2C_MXC_I2C4 option to the board config.
    
    Signed-off-by: Ilya Ledvich <ilya@compulab.co.il>

--- include/configs/cl_som_imx7.h
+++ include/configs/cl_som_imx7.h
@@ -54,6 +54,7 @@
 #define CONFIG_SYS_I2C
 #define CONFIG_SYS_I2C_MXC
 #define CONFIG_SYS_I2C_MXC_I2C2		/* enable I2C bus 2 */
+#define CONFIG_SYS_I2C_MXC_I2C4		/* enable I2C bus 4 */
 #define CONFIG_SYS_I2C_SPEED		100000
 
 #define CONFIG_SYS_I2C_EEPROM_ADDR	0x50
