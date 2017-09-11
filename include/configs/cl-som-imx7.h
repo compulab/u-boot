4718bacf5c1a48b41ab700aa6716b8e5d41362b5
Uri Mashiach <uri.mashiach@compulab.co.il>
2017-09-11 18:25:05 +0300

    arm: imx7d: cl-som-imx7: add support for usb keyboard
    Add support for usb keyboard for CL-SOM-iMX7.
    
    Signed-off-by: Uri Mashiach <uri.mashiach@compulab.co.il>

--- include/configs/cl-som-imx7.h
+++ include/configs/cl-som-imx7.h
@@ -129,7 +129,9 @@
 			"fi; " \
 		"fi;\0" \
 	"displaytype=dvi\0" \
-	"stdin=serial\0" \
+	"stdin=serial,usbkbd\0" \
+
+#define CONFIG_PREBOOT                 "usb start"
 
 #define CONFIG_BOOTCOMMAND \
 	"echo SD boot attempt ...; run sdbootscript; run sdboot; " \
@@ -195,6 +197,9 @@
 #define CONFIG_MXC_USB_PORTSC  (PORT_PTS_UTMI | PORT_PTS_PTW)
 #define CONFIG_MXC_USB_FLAGS   0
 #define CONFIG_USB_MAX_CONTROLLER_COUNT 2
+#define CONFIG_USB_KEYBOARD
+#define CONFIG_SYS_USB_EVENT_POLL_VIA_CONTROL_EP
+#define CONFIG_SYS_STDIO_DEREGISTER
 
 /* Uncomment to enable iMX thermal driver support */
 /*#define CONFIG_IMX_THERMAL*/
