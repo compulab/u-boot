245ded0a9049d1db425d121d38bf26e9019fed2f
Uri Mashiach <uri.mashiach@compulab.co.il>
2017-09-19 10:47:25 +0300

    compulab: eeprom: layout: add read serial function
    The function conver the CompuLab serial number format to tag_serialnr
    structure.
    
    Signed-off-by: Uri Mashiach <uri.mashiach@compulab.co.il>

--- include/eeprom_field.h
+++ include/eeprom_field.h
@@ -32,6 +32,9 @@ void eeprom_field_print_bin_rev(const struct eeprom_field *field, uchar *fbuf);
 int eeprom_field_update_bin_rev(struct eeprom_field *field, uchar *fbuf,
 				char *value);
 
+int eeprom_field_read_rev(const struct eeprom_field *field, uchar *fbuf,
+			  uchar *buf, int buf_size);
+
 void eeprom_field_print_mac(const struct eeprom_field *field, uchar *fbuf);
 int eeprom_field_update_mac(struct eeprom_field *field, uchar *fbuf,
 			    char *value);
