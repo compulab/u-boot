ea1ea8a961b371d066446c2f1ae4285e1b6889bd
Valentin Raevsky <valentin@compulab.co.il>
2020-08-05 00:22:29 +0300

    mcm-imx8m-mini: spl: Add board_fit_config_name_match()
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- board/compulab/mcm-imx8m-mini/spl.c
+++ board/compulab/mcm-imx8m-mini/spl.c
@@ -7,3 +7,13 @@
 int spl_board_private_init(void) {
 	return 0;
 }
+
+#ifdef CONFIG_SPL_LOAD_FIT
+int board_fit_config_name_match(const char *name)
+{
+	/* Just empty function now - can't decide what to choose */
+	debug("%s: %s\n", __func__, name);
+
+	return 0;
+}
+#endif
